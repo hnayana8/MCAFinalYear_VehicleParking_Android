ad4cf54a7fb0e2aec31ce86acbc1f51e8c4666ef
unknown <you@example.com>
2023-12-24 11:46:55 +0530

    Vehicle Parking Project Code

---
+++ app/src/main/java/com/example/vehicleparking/Confirmation.java
@@ -0,0 +1,46 @@
+package com.example.vehicleparking;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Confirmation extends AppCompatActivity {
+
+    private EditText nameEditText;
+    private EditText phoneEditText;
+    private EditText dateEditText;
+    private EditText timeEditText;
+    private Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.confirmation_details);
+
+        nameEditText = findViewById(R.id.editTextText3);
+        phoneEditText = findViewById(R.id.editTextPhone8);
+        dateEditText = findViewById(R.id.editTextDate);
+        timeEditText = findViewById(R.id.editTextTime2);
+        submitButton = findViewById(R.id.button8);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameEditText.getText().toString();
+                String phone = phoneEditText.getText().toString();
+                String date = dateEditText.getText().toString();
+                String time = timeEditText.getText().toString();
+
+                // No need to check if fields are empty
+
+                Intent intent = new Intent(Confirmation.this, dashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
